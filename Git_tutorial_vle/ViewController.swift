f27714fb384480a3d520d70c5e89bc79c9146cb4
en2455926 <en2455926@gmail.com>
2018-01-05 13:00:30 +0200

    One more text line added.

--- Git_tutorial_vle/ViewController.swift
+++ Git_tutorial_vle/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         print("hello text added!!! !!! !!!")
         print("New text line added.")
+        print("One more text line added.")
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
